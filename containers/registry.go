18a2bb6c88a17b109c2f5c9e18f96ebedf13a59a
vyas-viz <vyas@vizares.com>
2025-02-01 03:07:09 +0530

    update registry

--- containers/registry.go
+++ containers/registry.go
@@ -399,7 +399,7 @@ func calcId(cg *cgroup.Cgroup, md *ContainerMetadata) ContainerID {
 	case cgroup.ContainerTypeTalosRuntime:
 		return ContainerID(cg.ContainerId)
 	case cgroup.ContainerTypeStandaloneProcess:
-		return ContainerID(fmt.Sprintf("/standalone/%s", cg.ContainerId))
+		return ContainerID(fmt.Sprintf("/standalone/%s", md.name))
 	case cgroup.ContainerTypeDocker, cgroup.ContainerTypeContainerd, cgroup.ContainerTypeSandbox, cgroup.ContainerTypeCrio:
 	default:
 		return ""
