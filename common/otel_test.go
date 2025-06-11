37170a0c6bdb40ff8fe2b2a1229fa3c056c141d0
Shibani <shiibs@Shibanis-MacBook-Air.local>
2025-06-11 14:33:01 +0530

    fixed agent name in otel

--- common/otel_test.go
+++ common/otel_test.go
@@ -13,12 +13,12 @@ func TestContainerIdToServiceName(t *testing.T) {
 		"/k8s/otel-demo/otel-demo-frauddetectionservice")
 
 	assert.Equal(t,
-		f("/k8s/coroot/coroot-node-agent-np9pk/node-agent"),
-		"/k8s/coroot/coroot-node-agent")
+		f("/k8s/codexray/codexray-node-agent-np9pk/node-agent"),
+		"/k8s/codexray/codexray-node-agent")
 
 	assert.Equal(t,
-		f("/k8s/coroot/pyroscope-df884bb79-hhxtv/pyroscope"),
-		"/k8s/coroot/pyroscope")
+		f("/k8s/codexray/pyroscope-df884bb79-hhxtv/pyroscope"),
+		"/k8s/codexray/pyroscope")
 
 	assert.Equal(t,
 		f("/k8s/default/cassandra-main-12/cassandra"),
